f418cafb6a24cecab07c87031e136c3dd0763d3b
unknown <tjammieko@gmail.com>
2024-10-02 18:34:09 -0700

    formatting & task 5 6.5/7

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 class LobbyView {
-	constructor() { // TODO: Change <a> to be the whole box after we get assn1 evalutated
-		this.elem = createDOM(`
+  constructor() { // TODO: Change <a> to be the whole box after we get assn1 evalutated
+    this.elem = createDOM(`
 			<div class="content">
 				<ul class="room-list">
 					<li>
@@ -21,38 +21,76 @@ class LobbyView {
 				</div>
 			</div>
 		`);
-		this.listElem = this.elem.querySelector('ul.room-list');
-		this.inputElem = this.elem.querySelector('input');
-		this.buttonElem = this.elem.querySelector('button');
-	}
+    this.listElem = this.elem.querySelector('ul.room-list');
+    this.inputElem = this.elem.querySelector('input');
+    this.buttonElem = this.elem.querySelector('button');
+  }
 }
 
 
-  class Room {
-    constructor(id, name, image = "assets/everyone-icon.png", messages = []) { 
-      this.id = id;
-      this.name = name;
-      this.image = image;
-      this.messages = messages;
-    };      
-    
-    addMessage(username, text){
-      if (text == "" || text.trim().length == 0) //if text is empty or only whitespaces
-        return;
-      else {
-        const message = {
-          username: username,
-          text: text,
-        };
-        this.messages.push(message);
-        return;
-      }
+class Room {
+  constructor(id, name, image = "assets/everyone-icon.png", messages = []) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.messages = messages;
+    console.log("Creating New room with id:" + id);
+  };
+
+  addMessage(username, text) {
+    if (text == "" || text.trim().length == 0) //if text is empty or only whitespaces
+      return;
+    else {
+      const message = {
+        username: username,
+        text: text,
+      };
+      this.messages.push(message);
+      return;
     }
   }
-  
-  class ChatView {
-	constructor() {
-		this.elem = createDOM(`
+}
+
+class Lobby {
+  constructor() {
+    console.log("Creating New Lobby");
+    this.rooms = [];
+    this.rooms.push(new Room(1, `Room 1`));
+    this.rooms.push(new Room(2, `Room 2`));
+    this.rooms.push(new Room(3, `Room 3`));
+    this.rooms.push(new Room(4, `Room 4`));
+    };
+
+  getRoom(roomId) {
+    if (this.rooms[roomId]) {
+      return this.rooms[roomId];
+    }
+    else
+      return null;
+  }
+
+  //broken. not adding a room correctly.
+  addRoom(id, name, image, messages) {
+    console.log("Rooms before: ");
+    console.log(this.rooms);
+    console.log("# of rooms: " + this.rooms.length);
+    if (this.getRoom(id)) {
+      console.log("Could not create room: Room with this ID already exists");
+      return;
+    }
+    else {
+      this.rooms.push(new Room(id, name, image, messages));
+      console.log("Rooms after: ");
+      console.log(this.rooms);
+      console.log("# of rooms: " + this.rooms.length);
+      return;
+    }
+  }
+}
+
+class ChatView {
+  constructor() {
+    this.elem = createDOM(`
 			<div class="content">
 				<h4 class="room-name">Room Name</h4>
 				<div class="message-list">
@@ -71,15 +109,15 @@ class LobbyView {
 				</div>
 			</div>
 			`);
-		this.titleElem = this.elem.querySelector('h4.room-name');
-		this.chatElem = this.elem.querySelector('div.message-list');
-		this.inputElem = this.elem.querySelector('textarea');
-		this.buttonElem = this.elem.querySelector('button');
-	}
+    this.titleElem = this.elem.querySelector('h4.room-name');
+    this.chatElem = this.elem.querySelector('div.message-list');
+    this.inputElem = this.elem.querySelector('textarea');
+    this.buttonElem = this.elem.querySelector('button');
   }
-  class ProfileView {
-	constructor() {
-	  this.elem = createDOM(`
+}
+class ProfileView {
+  constructor() {
+    this.elem = createDOM(`
 		<div class="content">
 			<div class="profile-form">
 				<div class="form-field">
@@ -97,75 +135,75 @@ class LobbyView {
 			</div>
 		</div>
 		`);
-	}
   }
+}
 
 // Helper Functions
 
 // Removes the contents of the given DOM element (equivalent to elem.innerHTML = '' but faster)
 function emptyDOM(elem) {
-	while (elem.firstChild) elem.removeChild(elem.firstChild);
+  while (elem.firstChild) elem.removeChild(elem.firstChild);
 }
 
 // Creates a DOM element from the given HTML string
 function createDOM(htmlString) {
-	console.log("createDOM");
-	console.log(htmlString);
-	let template = document.createElement('template');
-	template.innerHTML = htmlString.trim();
-	console.log(template.content.firstChild);
-	return template.content.firstChild;
+  console.log("createDOM");
+  console.log(htmlString);
+  let template = document.createElement('template');
+  template.innerHTML = htmlString.trim();
+  console.log(template.content.firstChild);
+  return template.content.firstChild;
 }
 
 function main() {
-    console.log("page is fully loaded");
-	const lobbyView = new LobbyView();
-	const chatView = new ChatView();
-	const profileView = new ProfileView();
+  console.log("page is fully loaded");
+  const lobbyView = new LobbyView();
+  const chatView = new ChatView();
+  const profileView = new ProfileView();
 
-    renderRoute();
+  renderRoute();
 
-    function renderRoute() {
+  function renderRoute() {
 
-		const path = window.location.hash.substring(2);
-        const url = path.split('/')[0];
-        const pageView = document.getElementById('page-view');
+    const path = window.location.hash.substring(2);
+    const url = path.split('/')[0];
+    const pageView = document.getElementById('page-view');
 
-        if (url == "") {
-            console.log("indexContent");
-            //console.log(indexContent);
-            emptyDOM(pageView);
-            pageView.appendChild(lobbyView.elem);
-
-        }
-
-        else if (url == "profile") {
-            console.log("profileContent");
-            //console.log(profileContent);
-            emptyDOM(pageView);
-            pageView.appendChild(profileView.elem);
-
-        }
-
-        else if (url == "chat") {
-            console.log("chatContent");
-            //console.log(chatContent);
-            emptyDOM(pageView);
-            pageView.appendChild(chatView.elem);
-
-        }
+    if (url == "") {
+      console.log("indexContent");
+      //console.log(indexContent);
+      emptyDOM(pageView);
+      pageView.appendChild(lobbyView.elem);
 
     }
 
-    window.addEventListener('popstate', renderRoute);
-    window.addEventListener('hashchange', renderRoute);
+    else if (url == "profile") {
+      console.log("profileContent");
+      //console.log(profileContent);
+      emptyDOM(pageView);
+      pageView.appendChild(profileView.elem);
 
-    cpen322.export(arguments.callee, {
-        renderRoute: renderRoute,
-        lobbyView: lobbyView,
-		chatView: chatView,
-		profileView: profileView
-    });
+    }
+
+    else if (url == "chat") {
+      console.log("chatContent");
+      //console.log(chatContent);
+      emptyDOM(pageView);
+      pageView.appendChild(chatView.elem);
+
+    }
+
+  }
+
+  window.addEventListener('popstate', renderRoute);
+  window.addEventListener('hashchange', renderRoute);
+
+  cpen322.export(arguments.callee, {
+    renderRoute: renderRoute,
+    lobbyView: lobbyView,
+    chatView: chatView,
+    profileView: profileView
+  });
 }
 
 
