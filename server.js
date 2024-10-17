776e415aa275c31dd319d7c86449174551450d5d
dean.mccarthy <deancmccarthy@gmail.com>
2024-10-17 13:28:20 -0700

    Task 2 4/4

--- server.js
+++ server.js
@@ -13,6 +13,25 @@ const host = 'localhost';
 const port = 3000;
 const clientApp = path.join(__dirname, 'client');
 
+var chatrooms = [ //A3 T2A
+	{
+		id: 'room-1',
+		name: "Gaming",
+		image: 'client/assets/minecraft.jpg'
+	},
+	{
+		id: 'room-2',
+		name: "Class",
+		image: 'client/assets/everyone-icon.jpg'
+	},
+]
+
+var messages = {};
+
+chatrooms.forEach(currRoom => { //A3 T2B
+	messages[currRoom.id] = [];
+})
+
 // express app
 let app = express();
 
@@ -26,6 +45,22 @@ app.listen(port, () => {
 	console.log(`${new Date()}  App Started. Listening on ${host}:${port}, serving ${clientApp}`);
 });
 
+app.route('/chat')
+	.get((req,res) => {
+		const chats = chatrooms.map(room => ({
+			id: room.id,
+			name: room.name,
+			image: room.image,
+			messages: messages[room.id]
+		}));
+		res.json(chats);
+	});
+	
+
 // at the very end of server.js
 cpen322.connect('http://3.98.223.41/cpen322/test-a3-server.js');
-cpen322.export(__filename, { app });
+cpen322.export(__filename, { 
+	app,
+	messages,
+	chatrooms,
+ });
